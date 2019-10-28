a6775b550bc15e99b990ee6c092ab809740acb0a
Gabe Scholz <scholz.gabe@gmail.com>
2019-10-27 22:23:49 -0700

    _

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom";
 import { App } from "./components/app";
 
 if (process.env.NODE_ENV === "production" && navigator.serviceWorker) {
-  navigator.serviceWorker.register("./service-worker.ts", { scope: "/src/" });
+  navigator.serviceWorker.register("./service-worker.ts", {
+    scope: "/assets/"
+  });
 }
 
 const element = document.getElementById("root");
